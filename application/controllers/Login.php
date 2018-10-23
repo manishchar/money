1638daf08107bbd07a6d0d47d17cfc160c9aaf21
unknown <manish.alina@apalya.com>
2018-10-23 15:51:47 +0530

    login controller commet

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
+	// index controller
 	public function index()
 	{
 		$this->load->view('login');
