d47d737992145aebdcf4c6345851824989c39ce6
unknown <manish.alina@apalya.com>
2018-10-23 15:55:33 +0530

    initial commit

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 	// index controller
+	// index controller
 	public function index()
 	{
 		$this->load->view('login');
